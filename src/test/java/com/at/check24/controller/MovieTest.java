32d9272a2d2eb9ffba24ea5f72fe9625e438bb8d
mahsa.saeedi <mahsa.saeedy@gmail.com>
2022-09-27 11:58:02 +0330

    changed yaml file and eliminated unused imports

--- src/test/java/com/at/check24/controller/MovieTest.java
+++ src/test/java/com/at/check24/controller/MovieTest.java
@@ -14,9 +14,6 @@ import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import java.util.ArrayList;
 import java.util.List;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
